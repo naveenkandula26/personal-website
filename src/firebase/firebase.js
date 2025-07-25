10413a0035a5f3b86e9201e0678f8bdbf7e983c4
naveenkandula26 <naveenkandula26@gmail.com>
2025-07-25 18:11:20 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -11,7 +11,9 @@ const firebaseConfig = {
   appId: "1:891751225530:web:99a2544b4a9b8fd7448a83"
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase only once
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 
