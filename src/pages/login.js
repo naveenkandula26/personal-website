5b7da7c68ade24e966c2f23ddf0aa0dfe3c6602d
naveenkandula26 <naveenkandula26@gmail.com>
2025-07-25 16:46:52 +0530

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import LoginForm from '../components/LoginForm';
-import OTPForm from '../components/OTPForm';
-import '../styles/login.css';
-
-const LoginPage = () => {
-  return (
-    <div className="login-page">
-      <h2>Login</h2>
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { onAuthStateChanged } from 'firebase/auth <h2>Login</h2>
       <LoginForm />
       <OTPForm />
     </div>
@@ -14,3 +8,25 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase/firebase';
+import LoginForm from '../components/LoginForm';
+import OTPForm from '../components/OTPForm';
+import '../styles/login.css';
+
+const LoginPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.push('/home');
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  return (
+    <div className="login-page">
+     
